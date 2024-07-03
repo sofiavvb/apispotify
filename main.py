321fbb97390a87d7501ff2b57c90f0bbc5cfc2ed
sofiavvb <s252974@dac.unicamp.br>
2024-07-03 18:06:31 -0300

    update getTracksFromAlbum limit

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def get_artist_albums(token, artist_id):
     return json_result
 
 def get_album_tracks(token, album_id):
-    url = f"https://api.spotify.com/v1/albums/{album_id}/tracks?&market=US"
+    url = f"https://api.spotify.com/v1/albums/{album_id}/tracks?&market=US&limit=50"
     headers = get_auth_header(token)
     result = get(url, headers=headers)
     json_result = json.loads(result.content)
